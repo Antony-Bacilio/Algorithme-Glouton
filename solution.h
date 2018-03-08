d77cc96d8fad3e41f3df7605fd7f2d60586e326f
Antony-Bacilio <antonybaciliosalazar@gmail.com>
2018-03-09 00:00:12 +0100

    head fonctions

--- solution.h
+++ solution.h
@@ -1,6 +1,3 @@
-int *glouton_fonct_modifie(int *slu,int *tmin,int *tmax,int **mt,int nb);
-int *insert(int *tmp,int ins_nub,int nb);
-int evaluer(int *slu,int *tmin,int *tmax,int **mt,int nb);
-int *intdup(int *des,int *sor,int nb);
+int evaluer(int *tab,int *tmin,int *tmax,int **mt,int nb);
 void affiche(int *tab,int nb);
 int *glouton_fonct(int *tmin,int *tmax,int **mt,int nb);
